f8f08a2522618eb345224fad2e4961c21201870b
fmixing <alicekoroleva239@gmail.com>
2018-05-16 16:43:14 +0300

    Completed umatch, urestrict, urelax

--- src/Data/Union.hs
+++ src/Data/Union.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE DataKinds, TypeOperators, TypeFamilies, GADTs, PolyKinds, UndecidableInstances #-}
-{-# LANGUAGE FlexibleContexts, AllowAmbiguousTypes, RankNTypes, EmptyDataDecls, ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE FlexibleContexts, AllowAmbiguousTypes, RankNTypes, EmptyDataDecls, ScopedTypeVariables, TypeApplications, FlexibleInstances #-}
 
-module Data.Union (Data.Union.Set, Union(MkUnion), ulift, umatch) where
+module Data.Union (Data.Union.Set, Union(MkUnion), ulift, umatch, urelax, urestrict) where
 
 import GHC.TypeLits
-import Data.Type.Set hiding (Union)
+import Data.Type.Set hiding (Union, Subset, Set)
 import Data.Kind (Type)
 import Data.Type.Equality 
 import Data.Type.Bool
-import Data.Typeable (Typeable, typeOf, typeRep)
+import Type.Reflection (Typeable, typeOf, typeRep, SomeTypeRep(..))
 import Data.Functor.Identity (Identity(..))
+import Unsafe.Coerce (unsafeCoerce)
+import Data.Set as S (Set, empty, member, insert)
 
 type instance Cmp (n :: k) (m :: k) = BetterCmpType n m
 
@@ -29,24 +31,48 @@ type family IsElemForLists (x :: k) (xs :: [k]) :: Bool where
   IsElemForLists x '[] = 'False
   IsElemForLists y (x : xs) = (y == x) || IsElemForLists y xs
 
+-- Первое множество является сабсетом второго
+type family IsSubset (xs :: TSet k) (ys :: TSet k) :: Bool where 
+    IsSubset (MkTSet xs) (MkTSet ys) = IsSubsetForLists xs ys
+
+type family IsSubsetForLists (xs :: [k]) (ys :: [k]) :: Bool where 
+    IsSubsetForLists '[] ys = 'True
+    IsSubsetForLists xs '[] = 'False
+    IsSubsetForLists (x : xs) (y : ys) = If (x == y) (IsSubsetForLists xs ys) (IsSubsetForLists (x : xs) ys)
+
 data Union :: (k -> *) -> TSet k -> * where
   MkUnion :: (Typeable a, IsElem a xs ~ True) => f a -> Union f xs
 
--- 1. ulift взять элемент и обернуть его в union, например из f Int получить Union f [Int, Bool, Char]
--- 2. umatch это наоборот, взять union и попробовать скастовать его в элемент, например из Union f [Int, Bool, Char] 
---    мы можем попробовать получить f Int, f Bool, или f Char. Результат будет Just, если там ожидаемый тип, и Nothing, 
---    если какой-то другой. Попытка достать что-то не из этого списка типов должна на этапе компиляции отлавливаться.
--- 3. urestrict это попробовать скастовать union с некоторыми элементами в union с подмножеством этих элементов, 
---    такое обобщение umatch. Например, можем из Union f '[Int, Bool, Char] попробовать получить Union f '[Bool, Char].
--- 4. urelax наоборот, добавление элементов в список возможных
-
--- openUnion # (5 :: Int) это аналог ulift (Identity (5 :: Int)) :: Union Identity '[Bool, Int]
--- a ^? openUnion :: Maybe Int это аналог umatch a :: Maybe (Identity Int)
-
+-- ulift взять элемент и обернуть его в union, например из f Int получить Union f [Int, Bool, Char]
+-- union = ulift (Identity True) :: Union Identity (Data.Union.Set '[Int, Bool, Char])
 ulift :: (Typeable a, IsElem a as ~ True) => f a -> Union f as
 ulift = MkUnion
 
+-- umatch это наоборот, взять union и попробовать скастовать его в элемент, например из Union f [Int, Bool, Char] 
+--    мы можем попробовать получить f Int, f Bool, или f Char. Результат будет Just, если там ожидаемый тип, и Nothing, 
+--    если какой-то другой. Попытка достать что-то не из этого списка типов должна на этапе компиляции отлавливаться.
 -- Union f [Int, Bool, Char] -> Just (f k ::Int) | Just (f k :: Bool) | Just (f k :: Char) | Nothing
 umatch :: forall f a as. (Typeable a, IsElem a as ~ True) => Union f as -> Maybe (f a)
 umatch (MkUnion (fx :: f x)) =
   (\Refl -> fx) <$> testEquality (typeRep @a) (typeRep @x)
+
+-- добавление новых элементов
+urelax :: IsSubset xs ys ~ 'True => Union f xs -> Union f ys
+urelax = unsafeCoerce
+
+-- сужение множества. Maybe появляется из-за того, что может в итоге не получиться Union – убрали элемент a из него 
+-- => не выполняется IsElem
+urestrict :: forall f xs ys. (KnownTSet xs, IsSubset xs ys ~ 'True) => Union f ys -> Maybe (Union f xs)
+urestrict union@(MkUnion (fx :: f x)) = 
+    if (S.member (SomeTypeRep (typeRep @x)) (knownTSet @_ @xs)) 
+    then Just $ unsafeCoerce union 
+    else Nothing
+
+class KnownTSet (xs :: TSet k) where
+  knownTSet :: S.Set SomeTypeRep
+
+instance KnownTSet (MkTSet '[]) where
+  knownTSet = S.empty
+
+instance (KnownTSet (MkTSet xs), Typeable x) => KnownTSet (MkTSet (x:xs)) where
+  knownTSet = S.insert (SomeTypeRep (typeRep @x)) (knownTSet @_ @(MkTSet xs))
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,7 +8,7 @@ import Data.Type.Equality
 import Data.Type.Bool
 import Data.Typeable (Typeable, typeOf)
 import Data.Functor.Identity (Identity(..))
-import Data.Union (Set, Union (MkUnion), ulift, umatch)
+import Data.Union (Set, Union (MkUnion), ulift, umatch, urelax, urestrict)
 
 a :: Union Identity (Data.Union.Set '[Int, Bool, Char])
 a = MkUnion (Identity True)
@@ -23,6 +23,31 @@ foo = asSet $ Ext 'x' (Ext True Empty)
 
 test :: Proxy (Nub (Sort '[Int, Bool, Char, Bool, Int])) -> Proxy (Nub (Sort '[Int, Bool, Char]))
 test = id
+
+-- union = ulift (Identity True) :: Union Identity (Data.Union.Set '[Int, Bool, Char])
+
+-- umatch union :: Maybe (Identity Bool)
+-- Just (Identity True)
+
+-- umatch union :: Maybe (Identity Char)
+-- Nothing
+
+-- p = urelax union :: Union Identity (Data.Union.Set '[Int, Bool, Char, Integer])
+
+-- :t p
+-- p :: Union
+--        Identity ('Data.Union.MkTSet '[Bool, Char, Int, Integer])
+
+-- urelax union :: Union Identity (Data.Union.Set '[Int, Bool])
+
+-- Couldn't match type ‘'False’ with ‘'True’
+--         arising from a use of ‘urelax’
+
+-- isJust $ (urestrict union :: Maybe (Union Identity (Data.Union.Set '[Int])))
+-- False
+-- isJust $ (urestrict union :: Maybe (Union Identity (Data.Union.Set '[Int, Bool])))
+-- True
+
 
 main :: IO ()
 main = do
